1afa2c46f1c71c9279b4eb7856776a6633cec053
vinnyfs89 <viniciusfesil@gmail.com>
2019-05-05 23:01:50 -0300

    [ADD] Commit inicial.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import './plugins/vuetify'
+import ComunicationWebapp from '@vinnyfs89/comunication-webapp/src/index'
 import App from './App.vue'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  }
+});
+
+Vue.use(ComunicationWebapp, { store });
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
